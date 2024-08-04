a1540cc2431d64618f50dca12ee982303d5e4914
eraselk <eraselk@proton.me>
2024-08-04 03:44:59 +0000

    update download.sh

--- download.sh
+++ download.sh
@@ -4,4 +4,4 @@ read -p "Download Link: " link
 read -p "Output: " output
 echo
 echo "Downloading $output..."
-( echo "wget -q 'Dummy' -O 'Dummy2'" | sed "s/Dummy/$link" | sed "s/Dummy2/$output" | bash; ) && ( echo "Done"; ) || ( echo "Failed"; exit 1; )
+wget -q "$link" -O "$output" && echo "Done" || echo "Failed"; exit 1
